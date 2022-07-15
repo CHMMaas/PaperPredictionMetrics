aec0b5f95d20e6736746b445bfde94ebca77c191
Carolien Maas <c.h.m.maas@erasmusmc.nl>
2022-07-15 13:52:38 +0200

    Final results

--- Code/run.all.R
+++ Code/run.all.R
@@ -27,8 +27,8 @@ set.seed(1)
 # set parameters
 alpha.reg <- 0                # Ridge (alpha.reg=0), Lasso (alpha.reg=1), Elastic Net (alpha.reg=0.5) with no shrinkage on treatment arm indicator
 folds <- 5                    # number of folds to do cross validation on training/test and training/validation set (folds=5)
-treatment.arm <- 'met'       # life or met
-compute.new.results <- TRUE   # compute new results or not
+treatment.arm <- 'life'       # life or met
+compute.new.results <- FALSE   # compute new results or not
 
 #####
 ##### Simulations
